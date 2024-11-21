8c096bc58a78c14a1aa3c57d4935855679fb281b
Ross Brierley <115773271+rossbrierley@users.noreply.github.com>
2024-11-20 21:45:07 -0500

    Update function.cs

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -1,37 +1,41 @@
 using System.Text;
+using System.Net.Http;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace EsepWebhook;
-
-public class Function
+namespace EsepWebhook
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-    public string FunctionHandler(object input, ILambdaContext context)
+    public class Function
     {
-        context.Logger.LogInformation($"FunctionHandler received: {input}");
-
-        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
-        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
-        
-        var client = new HttpClient();
-        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+        public string FunctionHandler(object input, ILambdaContext context)
         {
-            Content = new StringContent(payload, Encoding.UTF8, "application/json")
-        };
-    
-        var response = client.Send(webRequest);
-        using var reader = new StreamReader(response.Content.ReadAsStream());
-            
-        return reader.ReadToEnd();
+            context.Logger.LogInformation($"FunctionHandler received: {input}");
+
+            // Deserialize the input JSON dynamically
+            dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+            string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+            {
+                Content = new StringContent(payload, Encoding.UTF8, "application/json")
+            };
+
+            try
+            {
+                var response = client.Send(request);
+                response.EnsureSuccessStatusCode();
+                using var reader = new StreamReader(response.Content.ReadAsStream());
+                return reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                context.Logger.LogError($"Failed to send Slack notification: {ex.Message}");
+                throw;
+            }
+        }
     }
 }
